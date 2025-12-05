IntersectMBO/ouroboros-network

dmq: added nodeToNodeCodecCBORTerm test

decentralized-message-queue/test/Main.hs
@@ -1,4 +1,18 @@
 module Main (main) where
 
+import Main.Utf8 (withUtf8)
+
+import Test.DMQ.NodeToNode qualified
+
+import Test.Tasty
+
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = withUtf8 $ defaultMain tests
+
+
+tests :: TestTree
+tests =
+  testGroup "decentralised-message-queue:testse"
+  [ Test.DMQ.NodeToNode.tests
+  ]

IntersectMBO/dmq-node

decentralized-message-queue/test/Main.hs
@@ -1,4 +1,18 @@
 module Main (main) where
 
+import Main.Utf8 (withUtf8)
+
+import Test.DMQ.NodeToNode qualified
+
+import Test.Tasty
+
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = withUtf8 $ defaultMain tests
+
+
+tests :: TestTree
+tests =
+  testGroup "decentralised-message-queue:tests"
+  [ Test.DMQ.NodeToNode.tests
+  ]