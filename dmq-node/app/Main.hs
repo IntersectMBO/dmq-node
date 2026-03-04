b936c88d6f3224afa822b5e06901df2a0bc02882
Marcin Szamotulski <coot@coot.me>
2026-03-04 09:09:10 +0100

    Connect to cardano-node in dmq kernel.

--- dmq-node/app/Main.hs
+++ dmq-node/app/Main.hs
@@ -51,7 +51,6 @@ import DMQ.Protocol.SigSubmission.Type (Sig (..))
 import DMQ.Tracer
 
 import DMQ.Diffusion.PeerSelection (policy)
-import DMQ.NodeToClient.LocalStateQueryClient
 import DMQ.Protocol.SigSubmission.Validate
 import Ouroboros.Network.Diffusion qualified as Diffusion
 import Ouroboros.Network.PeerSelection.LedgerPeers.Type
@@ -89,9 +88,7 @@ runDMQ commandLineConfig = do
           dmqcValidationTracer      = I validationTracer,
           dmqcLocalHandshakeTracer  = I localHandshakeTracer,
           dmqcCardanoNodeSocket     = I socketPath,
-          dmqcVersion               = I version,
-          dmqcLocalStateQueryTracer = I localStateQueryTracer,
-          dmqcLedgerPeers           = I ledgerPeers
+          dmqcVersion               = I version
         } = config' <> commandLineConfig
             `act`
             defaultConfiguration
@@ -133,20 +130,12 @@ runDMQ commandLineConfig = do
 
     -- TODO: this might not work, since `ouroboros-network` creates its own IO Completion Port.
     withIOManager \iocp -> do
-      let localSnocket'      = localSnocket iocp
-          mkStakePoolMonitor = connectToCardanoNode
-                                 (if localStateQueryTracer
-                                    then WithEventType "LocalStateQuery" >$< tracer
-                                    else nullTracer)
-                                 ledgerPeers
-                                 localSnocket'
-                                 socketPath
-
       withNodeKernel @StandardCrypto
+                     (localSnocket iocp)
+                     makeLocalBearer
                      tracer
                      dmqConfig
-                     psRng
-                     mkStakePoolMonitor $ \nodeKernel -> do
+                     psRng $ \nodeKernel -> do
         dmqDiffusionConfiguration <-
           mkDiffusionConfiguration dmqConfig nt nodeKernel.stakePools.ledgerBigPeersVar
 
