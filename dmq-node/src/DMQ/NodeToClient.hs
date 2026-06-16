b7d59834e52d46229bb3ff6e5595509eede730dd
Fabrizio Ferrai <fabrizio.ferrai@iohk.io>
2026-06-16 11:29:06 +0200

    Integration for Node release 11.1

--- dmq-node/src/DMQ/NodeToClient.hs
+++ dmq-node/src/DMQ/NodeToClient.hs
@@ -51,7 +51,7 @@ import Ouroboros.Network.Handshake.Queryable (Queryable (..))
 import Ouroboros.Network.Mux
 import Ouroboros.Network.OrphanInstances ()
 import Ouroboros.Network.Protocol.Handshake (HandshakeArguments (..))
-import Ouroboros.Network.Protocol.Handshake.Codec (cborTermVersionDataCodec,
+import Ouroboros.Network.Protocol.Handshake.Codec (mkVersionedCodecCBORTerm,
            codecHandshake, noTimeLimitsHandshake)
 import Ouroboros.Network.TxSubmission.Mempool.Reader
 import Ouroboros.Network.TxSubmission.Mempool.Simple
@@ -71,7 +71,7 @@ ntcHandshakeArguments tracer =
   , haBearerTracer     = nullTracer -- TODO
   , haHandshakeCodec   = codecHandshake nodeToClientVersionCodec
   , haVersionDataCodec =
-      cborTermVersionDataCodec
+      mkVersionedCodecCBORTerm
         nodeToClientCodecCBORTerm
   , haAcceptVersion = acceptableVersion
   , haQueryVersion  = queryVersion
